6a2fe50d9af3de655ad753813f8e50a3e57e77ac
Flc゛ <four_leaf_clover@foxmail.com>
2022-06-24 10:07:07 +0800

    feature(auth): SetAuth

--- auth.go
+++ auth.go
@@ -1,13 +1,16 @@
 package youdu
 
 import (
+	"crypto/md5"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"strconv"
 )
 
 const (
-	identifyUrl = "/cgi/identify"
+	identifyUrl    = "/cgi/identify"
+	userSetAuthUrl = "/cgi/user/setauth"
 )
 
 type Auth struct {
@@ -41,6 +44,7 @@ type IdentifyResp struct {
 	} `json:"userInfo"`
 }
 
+// Identify 单点登录
 func (a *Auth) Identify(token string) (i IdentifyResp, err error) {
 	resp, err := a.config.GetHttp().Get(identifyUrl, map[string]string{
 		"token": token,
@@ -59,4 +63,59 @@ func (a *Auth) Identify(token string) (i IdentifyResp, err error) {
 	}
 
 	return
+}
+
+type SetAuthResp struct {
+	FromUser   string `json:"fromUser"`
+	CreateTime int    `json:"createTime"`
+	PackageId  int    `json:"packageId"`
+	MsgType    string `json:"msgType"`
+	Passwd     string `json:"passwd"`
+}
+
+// SetAuth 第三方认证-设置认证信息
+func (a *Auth) SetAuth(userId, password string) (bool, error) {
+	accessToken, err := a.config.GetAccessTokenProvider().GetAccessToken()
+	if err != nil {
+		return false, err
+	}
+
+	bodyJson, err := json.Marshal(map[string]interface{}{
+		"userId":   userId,
+		"authType": 2,
+		"passwd":   fmt.Sprintf("%x", md5.Sum([]byte(password))),
+	})
+	if err != nil {
+		return false, err
+	}
+
+	encrypt, err := a.config.GetEncryptor().Encrypt(string(bodyJson))
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := a.config.GetHttp().Post(userSetAuthUrl+"?accessToken="+accessToken, map[string]interface{}{
+		"appId":   a.config.AppId,
+		"buin":    a.config.Buin,
+		"encrypt": encrypt,
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	if !resp.IsSuccess() {
+		return false, errors.New("Response status code is " + strconv.Itoa(resp.StatusCode()))
+	}
+
+	jsonRet, err := resp.Json()
+	if err != nil {
+		return false, err
+	}
+
+	if jsonRet["errcode"].(float64) != 0 {
+		return false, errors.New(jsonRet["errMsg"].(string))
+	}
+
+	return true, nil
 }
